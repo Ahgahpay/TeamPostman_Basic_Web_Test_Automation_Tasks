be46b9cba0cd440a94c4660a3e435fff97e383dd
Ahgahpay <agapeakpo@gmail.com>
2023-07-05 13:14:59 +0100

    Added MyFirstJavaProject

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("Hello there, I'm Agape and this is my first Java Project on IntelliJ!");
+
+            System.out.println("It's also my first time writing Java and i believe i will enjoy the experience");
+
+            System.out.println("Will keep updating as I learn, Cheers");
+        }
+    }
