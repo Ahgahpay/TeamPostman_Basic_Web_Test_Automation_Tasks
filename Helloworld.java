25ade9c6ef5acb0fb1778289c9343d74e94e34f0
Ahgahpay <agapeakpo@gmail.com>
2023-07-06 07:50:11 +0100

    Updated Java file

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+public class Helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello there, I'm Agape and this is my first Java Project on IntelliJ!");
+
+        System.out.println("It's also my first time writing Java and i believe i will enjoy the experience");
+
+        System.out.println("Will keep updating as I learn, Cheers");
+    }
+
+}
